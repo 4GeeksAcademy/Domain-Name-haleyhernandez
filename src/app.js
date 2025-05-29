9d10c81d05542932aeac3857f6e1b024261d73ae
Haley <haleymariehernandez10@gmail.com>
2025-05-29 01:04:41 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['cute', 'big'];
+  let noun = ['puppy', 'kitty'];
+  let extentison =['.com', '.net', '.org']
+
+  for(let elm of pronoun){
+    for(let elm2 of adj){
+      for(let elm3 of noun){
+        for(let elm4 of extentison){
+        
+        let result = elm + elm2 + elm3 + elm4
+        console.log(result);
+        document.querySelector('#name').innerHTML += "<ul>"+result+"</ul>";
+        }
+      }
+    }
+  }
+ console.log("Any string");
 };
